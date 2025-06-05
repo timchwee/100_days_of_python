e887a9770fcc5eb1b9de390edb60313ec7e5a48f
Tim Chwee <tim.chwee@outlook.com>
2025-06-05 22:16:15 +1000

    added 2 functions

--- 100_days_of_python/day8.py
+++ 100_days_of_python/day8.py
@@ -4,3 +4,19 @@ def greet():
     print("have you eaten?")
 
 greet()
+
+# Fuctions that allow for inputs
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"Hod do you do {name}")
+
+greet_with_name("Tim")
+
+# Functions with more thatn 1 input
+
+def greet_with(name, country):
+    print(f"Hellow {name}")
+    print(f"Are you from {country}")
+
+greet_with("Tim", "Australia")
